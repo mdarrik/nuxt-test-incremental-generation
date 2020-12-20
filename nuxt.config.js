7ed9a19771e4851691cec2574e98bd74f6c35ec2
Darrik <30670444+mdarrik@users.noreply.github.com>
2020-12-19 22:28:55 -0800

    init: initialize project

---
+++ nuxt.config.js
@@ -0,0 +1,55 @@
+import getRoutes from './get-routes.js'
+export default {
+  // Target (https://go.nuxtjs.dev/config-target)
+  target: 'static',
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'Incremental Nuxt Generate - NASA API',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    // https://go.nuxtjs.dev/chakra
+    '@chakra-ui/nuxt',
+    // https://go.nuxtjs.dev/emotion
+    '@nuxtjs/emotion',
+  ],
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {},
+
+  generate: {
+    cache: { ignore: ['incremental-generation-plugin', 'www'] },
+    routes() {
+      return getRoutes()
+    },
+    crawler: false,
+  },
+  chakra: {
+    config: {
+      autoImport: true,
+    },
+  },
+}
