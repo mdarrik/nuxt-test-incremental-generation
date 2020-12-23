e6822d60c0e0e93bd2319bae0ec7a0e2ba45064f
Darrik <30670444+mdarrik@users.noreply.github.com>
2020-12-22 22:06:45 -0800

    feat: updates layout

---
+++ components/about-aside.vue
@@ -0,0 +1,39 @@
+<template>
+  <CBox
+    as="aside"
+    border-width="2px"
+    rounded="lg"
+    :max-w="['md', 'md', 'xl']"
+    p="4"
+    shadow="sm"
+    bg="gray.50"
+  >
+    <p>
+      This project is mostly to explore incrementally generating pages with
+      Nuxt. It's set up so that a deploy to only build the page for today's
+      image can be triggered via a webhook.
+    </p>
+    <p>
+      You can
+      <CLink
+        href="https://github.com/mdarrik/nuxt-test-incremental-generation"
+        text-decoration="underline"
+        color="cyan.700"
+      >
+        see the source code here.
+      </CLink>
+      To read more about it, checkout my
+      <CLink
+        href="https://darrik.dev/nuxt-incremental-page-generation"
+        text-decoration="underline"
+        color="cyan.700"
+      >
+        article on incremental page generation with Nuxt
+      </CLink>
+    </p>
+  </CBox>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <CThemeProvider>
+      <CColorModeProvider>
+        <CBox font-family="body">
+          <CBox as="main">
+            <CReset />
+            <Nuxt />
+          </CBox>
+          <CFlex justify="center" mt="6">
+            <AboutAside />
+          </CFlex>
+        </CBox>
+      </CColorModeProvider>
+    </CThemeProvider>
+  </div>
+</template>
+<script>
+import {
+  CThemeProvider,
+  CColorModeProvider,
+  CReset,
+  CBox,
+} from '@chakra-ui/vue'
+
+export default {
+  name: 'App',
+  components: {
+    CThemeProvider,
+    CColorModeProvider,
+    CReset,
+    CBox,
+  },
+}
+</script>
